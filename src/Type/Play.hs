5f0c19ffa2b2204bbf0a7f7b149b6756a38b29b7
Mateusz Curylo <mhcurylo@gmail.com>
2017-12-12 20:23:43 +0000

    Play

--- src/Type/Play.hs
+++ src/Type/Play.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings        #-}
 
 module Type.Play (
   Play(..),
@@ -15,26 +16,56 @@ import Type.Date
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 (readInt)
 import Data.Text (Text, pack)
+import Data.Text.Encoding (encodeUtf8)
+import qualified Data.ByteString as B 
 import Data.Attoparsec.ByteString (Parser, parseOnly, takeWhile1, word8) 
 import Web.HttpApiData (FromHttpApiData, parseQueryParam)
 import Control.Arrow (left)
+import Test.QuickCheck (Gen, listOf1, elements, Arbitrary, arbitrary)
+import Data.Char (toUpper)
+import Control.Applicative (liftA2)
+
+arbitraryName :: Gen String
+arbitraryName = headUpper <$> listOf1 (elements "abcdefghijlmnopqrstuwxyz")
+  where
+  headUpper (x:xs) = toUpper x : xs
 
 newtype Player = Player {
   player :: ByteString
-} deriving (Show, Eq, Ord)
+} deriving (Eq, Ord)
+
+instance Show Player where
+  show  = show . player
+
+instance Arbitrary Player where
+  arbitrary = do
+    p <- liftA2 (++) arbitraryName arbitraryName 
+    return $ Player . encodeUtf8 . pack $ p
+
+isAlpha w = w >= 65 && w <= 122
+
+playerParser :: Parser Player
+playerParser = Player <$> takeWhile1 isAlpha
 
 newtype Score = Score {
   score :: Int
-} deriving (Show, Eq, Ord, Num)
+} deriving (Eq, Ord, Num)
+
+instance Show Score where
+  show  = show . score 
+
+instance Show 
 
 data Play = Play {
     pPlayer :: Player
   , pScore  :: Score  
   , pDate   :: Date 
-} deriving (Show, Eq)
+} deriving (Eq)
+
+instance Show Play where
+  show (Play p s d) = show p ++ "," ++ show s ++ "," ++ show d
 
 isDigit w = w >= 48 && w <= 57
-isAlpha w = w >= 65 && w <= 122
 eol = word8 10
 
 isPlayOld :: Date -> Play -> Bool
@@ -46,9 +77,6 @@ scoreParser = do
   case readInt d of 
     Just (d', _) -> return $ Score d'
     Nothing -> fail "Not a score"
-    
-playerParser :: Parser Player
-playerParser = Player <$> takeWhile1 isAlpha
 
 playParserEOL :: Parser Play
 playParserEOL = do
